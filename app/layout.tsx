119c16ecc081da9ef709e52b555e32866fd41ade
Shazeb Ramin Arafin <shazeb.arafin@gmail.com>
2025-09-18 20:28:20 +0400

    Created Navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,11 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
+import { SiteHeader } from "@/components/site-header";
+import { SectionCards } from "@/components/section-cards";
+import { ChartAreaInteractive } from "@/components/chart-area-interactive";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +32,20 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <SidebarProvider
+          style={
+            {
+              "--sidebar-width": "calc(var(--spacing) * 72)",
+              "--header-height": "calc(var(--spacing) * 12)",
+            } as React.CSSProperties
+          }
+        >
+          <AppSidebar variant="inset" />
+          <SidebarInset>
+            <SiteHeader />
+            {children}
+          </SidebarInset>
+        </SidebarProvider>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
+'use client'
 import { Button } from "@/components/ui/button";
+import { Clicker_Script } from "next/font/google";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Shazeb writes html Better</h1>
-      <Button>click me or else...</Button>
+    <div className="flex flex-1 flex-col">
+      <h1 className="h-1 font-bold  ">  My Page </h1>
     </div>
   );
 }
+
+
