c01d1d353f5d5ca296cd5e11f52b8fdaed3134aa
Shazeb Ramin Arafin <shazeb.arafin@gmail.com>
2025-09-23 20:28:52 +0400

    Myprofile (edited)

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,36 @@
 'use client'
 import { Button } from "@/components/ui/button";
+import { Card, CardAction, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { Clicker_Script } from "next/font/google";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="flex flex-1 flex-col">
-      <h1 className="h-1 font-bold  ">  My Page </h1>
+      <Label>Shazeb Arafin</Label>
+      <Card className="w-full max-w-sm bg-indigo-400">
+        <CardHeader>
+          <CardTitle>About Me</CardTitle>
+          {/* <CardDescription>
+          Enter your email below to login to your account
+        </CardDescription> */}
+        </CardHeader>
+        <CardContent>
+          <Image src={"/boy.jpg"} alt="boy" width={300} height={300}/>
+          <p> 
+            Hello my name is Shazeb... uhhh
+            I am 11 years old :D and I am in year 6 and uhmm
+            uhmm I don't know what to say uhmm I am from Bangledesh.
+            I love coding and swiming. I use VSCODE and Roblox studios for coding and yeah thats It.
+            uhh love eating pizza uhhmm I love Science and sometimes math.
+            I don't know why I am holding a camera and this is not how I look, I am way taller..
+          </p>
+        </CardContent>
+        <CardFooter className="flex-col gap-2">
+        </CardFooter>
+      </Card>
     </div>
   );
 }
--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -61,94 +61,18 @@ const data = {
       icon: IconFolder,
     },
     {
-      title: "Team",
+      title: "My Favourite book",
+      url: "#",
+      icon: IconUsers,
+    },
+    {
+      title: "My Message",
       url: "#",
       icon: IconUsers,
     },
   ],
-  navClouds: [
-    {
-      title: "Capture",
-      icon: IconCamera,
-      isActive: true,
-      url: "#",
-      items: [
-        {
-          title: "Active Proposals",
-          url: "#",
-        },
-        {
-          title: "Archived",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Proposal",
-      icon: IconFileDescription,
-      url: "#",
-      items: [
-        {
-          title: "Active Proposals",
-          url: "#",
-        },
-        {
-          title: "Archived",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Prompts",
-      icon: IconFileAi,
-      url: "#",
-      items: [
-        {
-          title: "Active Proposals",
-          url: "#",
-        },
-        {
-          title: "Archived",
-          url: "#",
-        },
-      ],
-    },
-  ],
-  navSecondary: [
-    {
-      title: "Settings",
-      url: "#",
-      icon: IconSettings,
-    },
-    {
-      title: "Get Help",
-      url: "#",
-      icon: IconHelp,
-    },
-    {
-      title: "Search",
-      url: "#",
-      icon: IconSearch,
-    },
-  ],
-  documents: [
-    {
-      name: "Data Library",
-      url: "#",
-      icon: IconDatabase,
-    },
-    {
-      name: "Reports",
-      url: "#",
-      icon: IconReport,
-    },
-    {
-      name: "Word Assistant",
-      url: "#",
-      icon: IconFileWord,
-    },
-  ],
-}
+
+ }
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
@@ -170,9 +94,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
       </SidebarHeader>
       <SidebarContent>
         <NavMain items={data.navMain} />
-        <NavDocuments items={data.documents} />
-        <NavSecondary items={data.navSecondary} className="mt-auto" />
-      </SidebarContent>
+       </SidebarContent>
       <SidebarFooter>
         <NavUser user={data.user} />
       </SidebarFooter>
