170e7c95f905a952a48fc84d04b85a7ec8ae11a3
Shazeb Ramin Arafin <shazeb.arafin@gmail.com>
2025-09-25 20:26:48 +0400

    Myprofile(editedx2)

--- app/page.tsx
+++ app/page.tsx
@@ -8,9 +8,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-1 flex-col">
+    <div className="flex flex-1 flex-col p-7 gap-5">
       <Label>Shazeb Arafin</Label>
-      <Card className="w-full max-w-sm bg-indigo-400">
+      <Card className="w-full shadow-2xl shadow-purple-800  bg-indigo-400 border-t-blue-700 border-b-blue-700   border-l-blue-300 
+       border-r-blue-300 border-8">
         <CardHeader>
           <CardTitle>About Me</CardTitle>
           {/* <CardDescription>
@@ -18,8 +19,9 @@ export default function Home() {
         </CardDescription> */}
         </CardHeader>
         <CardContent>
-          <Image src={"/boy.jpg"} alt="boy" width={300} height={300}/>
-          <p> 
+
+          <Image src={"/boy.jpg"} alt="boy" width={300} height={500} />
+          <p>
             Hello my name is Shazeb... uhhh
             I am 11 years old :D and I am in year 6 and uhmm
             uhmm I don't know what to say uhmm I am from Bangledesh.
@@ -31,7 +33,57 @@ export default function Home() {
         <CardFooter className="flex-col gap-2">
         </CardFooter>
       </Card>
+
+      {/* My City section */}
+      <Card className="w-full shadow-2xl shadow-yellow-300  bg-orange-200 border-t-orange-600 border-b-orange-600  border-l-orange-400
+       border-r-orange-400 border-8">
+        <CardHeader>
+          <CardTitle className="font-extrabold">My City</CardTitle>
+          {/* <CardDescription>
+          Enter your email below to login to your account
+        </CardDescription> */}
+        </CardHeader>
+        <CardContent>
+
+          <Image className="w-full shadow-2xl bg-orange-200 border-t-orange-400 border-b-orange-400  border-l-orange-600
+       border-r-orange-600 border-8" src={"/city.jpg"} alt="city" width={500} height={500} />
+          <p>
+            Dubai, located in the country of UAE(United  arab emirates)
+            has the most famous sights like the burj Khalifa, and  Dubai[a]
+            is the most populous city in the United Arab Emirates
+            and the capital of the Emirate of Dubai.[10] It is located
+            on a creek on the south-eastern coast of the Persian Gulf. As of 2025, the city population stands at 4 million
+          </p>
+        </CardContent>
+        <CardFooter className="flex-col gap-2">
+        </CardFooter>
+      </Card>
+
+      {/* Gallery section */}
+      <Card className="w-full shadow-2xl shadow-yellow-300  bg-orange-200 border-t-orange-600 border-b-orange-600  border-l-orange-400
+       border-r-orange-400 border-8">
+        <CardHeader>
+          <CardTitle>Gallery</CardTitle>
+          {/* <CardDescription>
+          Enter your email below to login to your account
+        </CardDescription> */}
+        </CardHeader>
+        <CardContent>
+
+          <Image src={"/city.jpg"} alt="city" width={500} height={500} />
+          <p>
+            Dubai, located in the country of UAE(United  arab emirates)
+            has the most famous sights like the burj Khalifa, and  Dubai[a]
+            is the most populous city in the United Arab Emirates
+            and the capital of the Emirate of Dubai.[10] It is located
+            on a creek on the south-eastern coast of the Persian Gulf. As of 2025, the city population stands at 4 million
+          </p>
+        </CardContent>
+        <CardFooter className="flex-col gap-2">
+        </CardFooter>
+      </Card>
     </div>
+
   );
 }
 
