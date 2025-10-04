f8193eac2bd16e00c3049fad8de4e5d85544d2bc
Shazeb Ramin Arafin <shazeb.arafin@gmail.com>
2025-10-04 18:24:09 +0400

    my-profile

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,9 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardAction, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
+import { ItemContent, ItemDescription, ItemHeader, ItemTitle } from "@/components/ui/item";
 import { Label } from "@/components/ui/label";
+import { Item } from "@radix-ui/react-toggle-group";
 import { Clicker_Script } from "next/font/google";
 import Image from "next/image";
 
@@ -68,30 +70,101 @@ export default function Home() {
         </CardDescription> */}
         </CardHeader>
         <CardContent>
-            <div className="grid gird-cols-3 grid-rows-3 gap-4">
-              <div className="bg-amber-300">
-                <div className="bg-red-500">1</div>
-                <div className="bg-red-500">2</div>
-                <div className="bg-red-500">3</div>
-              </div>
-              <div className="bg-amber-300">2</div>
-              <div className="bg-amber-300">3</div>
+          <div className="grid grid-flow-col grid-cols-4">
+            <div className="bg-blue-300">
+              <Image className=" aspect-6/5 object-cover w-full shadow-2xl bg-blue-200 border-t-blue-400 border-b-blue-400  border-l-blue-600 border-r-blue-600 border-8" src={"/Gall1.jpg"} alt="Gall1" width={500} height={500} />
             </div>
-          {/* <Image src={"/city.jpg"} alt="city" width={500} height={500} /> */}
-          <p>
-            Dubai, located in the country of UAE(United  arab emirates)
-            has the most famous sights like the burj Khalifa, and  Dubai[a]
-            is the most populous city in the United Arab Emirates
-            and the capital of the Emirate of Dubai.[10] It is located
-            on a creek on the south-eastern coast of the Persian Gulf. As of 2025, the city population stands at 4 million
-          </p>
+            <div className="bg-blue-600">
+              <Image className="aspect-6/5 object-cover w-full shadow-2xl bg-blue-200 border-t-blue-600 border-b-blue-600  border-l-blue-400 border-r-blue-400 border-8" src={"/Gall2.jpg"} alt="Gall2" width={500} height={500} />
+            </div>
+            <div className="bg-blue-600">
+              <Image className="aspect-6/5 object-contain w-full shadow-2xl bg-blue-200 border-t-blue-400 border-b-blue-400  border-l-blue-600 border-r-blue-600 border-8" src={"/Gall3.jpg"} alt="Gall3" width={300} height={300} />
+            </div>
+            <div className="bg-blue-600">
+              <Image className="aspect-6/5 object-contain w-contain shadow-2xl bg-blue-200 border-t-blue-400 border-b-blue-400  border-l-blue-600 border-r-blue-600 border-8" src={"/Gall4.jpg"} alt="Gall4" width={300} height={300} />
+            </div>
+          </div>
+
+          <div className="grid grid-flow-col grid-cols-4">
+            <div className="bg-blue-300">
+              <Image className=" aspect-6/5 object-cover w-full shadow-2xl bg-blue-200 border-t-blue-400 border-b-blue-400  border-l-blue-600 border-r-blue-600 border-8" src={"/Gall5.jpg"} alt="Gall5" width={500} height={500} />
+            </div>
+            <div className="bg-blue-600">
+              <Image className="aspect-6/5 object-cover w-full shadow-2xl bg-blue-200 border-t-blue-600 border-b-blue-600  border-l-blue-400 border-r-blue-400 border-8" src={"/Gall6.jpg"} alt="Gall6" width={500} height={500} />
+            </div>
+            <div className="bg-blue-600">
+              <Image className="aspect-6/5 object-contain w-full shadow-2xl bg-blue-200 border-t-blue-400 border-b-blue-400  border-l-blue-600 border-r-blue-600 border-8" src={"/boy.jpg"} alt="boy33" width={300} height={300} />
+            </div>
+            <div className="bg-blue-600">
+              <Image className="aspect-6/5 object-contain w-full shadow-2xl bg-blue-200 border-t-blue-400 border-b-blue-400  border-l-blue-600 border-r-blue-600 border-8" src={"/boy.jpeg"} alt="boy" width={300} height={300} />
+            </div>
+          </div>
         </CardContent>
-        <CardFooter className="flex-col gap-2">
+        <CardFooter>
+        </CardFooter>
+      </Card>
+
+      {/* Gallery section */}
+      <Card className="w-full shadow-2xl shadow-green-950 bg-white border-t-green-950 border-b-green-950 border-l-green-900
+       border-r-green-900 border-8">
+        <CardHeader>
+          <CardTitle>My favourite Books</CardTitle>
+          {/* <CardDescription>
+          Enter your email below to login to your account
+        </CardDescription> */}
+        </CardHeader>
+        <CardContent>
+          <div className="grid grid-flow-col grid-cols-3">
+            <div className="bg-green-300">
+              <Image className=" aspect-6/5 object-contain w-full shadow-2xl bg-green-200 border-t-green-400 border-b-green-400  border-l-green-600 border-r-green-600 border-8" src={"/HP.jpg"} alt="HP" width={500} height={500} />
+            </div>
+            <div className="bg-green-600">
+              <Image className="aspect-6/5 object-contain w-full shadow-2xl bg-green-200 border-t-green-600 border-b-green-600  border-l-green-400 border-r-green-400 border-8" src={"/ASOUE.jpg"} alt="ASOUE" width={500} height={500} />
+            </div>
+            <div className="bg-blue-600">
+              <Image className="aspect-6/5 object-contain w-full shadow-2xl bg-green-200 border-t-green-400 border-b-green-400  border-l-green-600 border-r-green-600 border-8" src={"/DWK.jpg"} alt="DWK" width={300} height={300} />
+            </div>
+          </div>
+
+        </CardContent>
+        <CardFooter>
+        </CardFooter>
+      </Card>
+
+      {/* Gallery section */}
+      <Card className="w-full shadow-2xl shadow-green-950 bg-white border-t-green-950 border-b-green-950 border-l-green-900
+       border-r-green-900 border-8">
+        <CardHeader>
+          <CardTitle>My favourite Sports</CardTitle>
+          {/* <CardDescription>
+          Enter your email below to login to your account
+        </CardDescription> */}
+        </CardHeader>
+        <CardContent>
+          <div className="grid grid-flow-col grid-cols-3">
+            <div className="bg-green-300">
+              <Image className=" aspect-6/5 object-contain w-full shadow-2xl bg-green-200 border-t-green-400 border-b-green-400  border-l-green-600 border-r-green-600 border-8" src={"/BasBall.jpg"} alt="BassBall" width={500} height={500} />
+              
+            </div>
+            <div className="bg-green-600">
+              <Image className="aspect-6/5 object-contain w-full shadow-2xl bg-green-200 border-t-green-600 border-b-green-600  border-l-green-400 border-r-green-400 border-8" src={"/VollBall.jpg"} alt="VollBall" width={500} height={500} />
+            </div>
+            <div className="bg-blue-600">
+              <Image className="aspect-6/5 object-contain w-full shadow-2xl bg-green-200 border-t-green-400 border-b-green-400  border-l-green-600 border-r-green-600 border-8" src={"/Swim.jpg"} alt="Swim" width={300} height={300} />
+            </div>
+          </div>
+          
+
+        </CardContent>
+        <CardFooter>
         </CardFooter>
       </Card>
     </div>
 
+
+
   );
 }
+
 
 
